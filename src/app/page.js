6abc82397b74fdb48daa30eb082c322e7cf4bd54
ruchu8 <o0o08l@outlook.com>
2024-10-26 12:49:47 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -504,7 +504,7 @@ export default function Home() {
               value={selectedOption} // 将选择框的值绑定到状态中的 selectedOption
               onChange={handleSelectChange} // 当选择框的值发生变化时触发 handleSelectChange 函数
               className="text-lg p-2 border  rounded text-center w-auto sm:w-auto md:w-auto lg:w-auto xl:w-auto  2xl:w-36">
-              <option value="tg" >cloudflare(会失效)</option>
+              <option value="tg" >TG(会失效)</option>
               <option value="qqkj">QQ空间</option>
               <option value="vviptuangou">vviptuangou</option>
               <option value="58img">58img</option>
