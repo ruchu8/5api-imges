5b145237ef48c98550988fff40ecdf4911a06bd7
ruchu8 <o0o08l@outlook.com>
2024-10-26 13:00:40 +0800

    Update route.js

--- src/app/api/ip/route.js
+++ src/app/api/ip/route.js
@@ -1,56 +1,34 @@
 import { NextResponse } from "next/server";
-import { headers } from 'next/headers';
+import { headers } from 'next/headers'
 import { getRequestContext } from '@cloudflare/next-on-pages';
 
 // ...
 
+
 export const runtime = 'edge';
 export async function GET(request) {
-  try {
-    // 获取客户端的IP地址
-    const xForwardedFor = request.headers.get('x-forwarded-for');
-    const xRealIp = request.headers.get('x-real-ip');
-    const remoteAddress = request.socket.remoteAddress;
+  // 获取客户端的IP地址
+  // const { env, cf, ctx } = getRequestContext();
+  const ip = request.headers.get('x-forwarded-for') || request.headers.get('x-real-ip') || request.socket.remoteAddress;
+  const clientIp = ip ? ip.split(',')[0].trim() : 'IP not found';
 
-    let ipList = [];
-    if (xForwardedFor) {
-      ipList = xForwardedFor.split(',').map(ip => ip.trim());
-    } else if (xRealIp) {
-      ipList = [xRealIp.trim()];
-    } else if (remoteAddress) {
-      ipList = [remoteAddress];
+  return new Response(
+    JSON.stringify({
+      ip: clientIp
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
     }
+  )
 
-    // 提取IPv4地址
-    const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
-    const clientIp = ipList.find(ip => ipv4Regex.test(ip)) || 'IP not found';
 
-    return new Response(
-      JSON.stringify({
-        ip: clientIp
-      }),
-      {
-        status: 200,
-        headers: {
-          'content-type': 'application/json',
-        },
-      }
-    );
-  } catch (error) {
-    console.error('Error fetching IP address:', error);
-    return new Response(
-      JSON.stringify({
-        error: 'Internal Server Error'
-      }),
-      {
-        status: 500,
-        headers: {
-          'content-type': 'application/json',
-        },
-      }
-    );
-  }
+
+
 }
+
 
 async function insertImageData(env, src, referer, ip, rating, time) {
   try {
@@ -59,6 +37,6 @@ async function insertImageData(env, src, referer, ip, rating, time) {
            VALUES ('${src}', '${referer}', '${ip}', ${rating}, 1, '${time}')`
     ).run();
   } catch (error) {
-    console.error('Error inserting image data:', error);
+
   }
 }
