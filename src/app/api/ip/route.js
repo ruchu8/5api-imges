d0c1a3c7d3e098fb9bc69d657702a8d621555ee6
ruchu8 <o0o08l@outlook.com>
2024-10-26 13:13:15 +0800

    Update route.js

--- src/app/api/ip/route.js
+++ src/app/api/ip/route.js
@@ -1,40 +1,27 @@
-import { NextResponse } from "next/server";
-import { headers } from 'next/headers'
-import { getRequestContext } from '@cloudflare/next-on-pages';
-
-// ...
-
 export const runtime = 'edge';
-export async function GET(request) {
-  const ip = request.headers.get('x-forwarded-for') || request.headers.get('x-real-ip') || request.socket.remoteAddress;
 
-  // 过滤出IPv4地址
-  const clientIp = ip 
-    ? ip.split(',')
-        .map(ip => ip.trim())
-        .find(ip => /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/.test(ip)) || 'IP not found' 
-    : 'IP not found';
+export async function GET(request) {
+  // 从请求头获取IP地址
+  const xForwardedFor = request.headers.get('x-forwarded-for');
+  const ip = xForwardedFor ? xForwardedFor.split(',')[0].trim() : 
+            request.headers.get('x-real-ip') || 
+            request.socket.remoteAddress;
+
+  // 验证并确保是IPv4地址
+  const isValidIPv4 = (ip) => {
+    const ipv4Pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){3}/;
+    return ipv4Pattern.test(ip);
+  }
+
+  const clientIp = isValidIPv4(ip) ? ip : 'IP not found';
 
   return new Response(
-    JSON.stringify({
-      ip: clientIp
-    }),
+    JSON.stringify({ ip: clientIp }),
     {
       status: 200,
       headers: {
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
       },
     }
   );
-}
-
-async function insertImageData(env, src, referer, ip, rating, time) {
-  try {
-    const instdata = await env.prepare(
-      `INSERT INTO imginfo (url, referer, ip, rating, total, time)
-           VALUES ('${src}', '${referer}', '${ip}', ${rating}, 1, '${time}')`
-    ).run();
-  } catch (error) {
-    // 处理错误
-  }
 }
