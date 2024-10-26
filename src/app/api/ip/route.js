b55888ba31520609d2db1635affb25598c0c03ab
ruchu8 <o0o08l@outlook.com>
2024-10-26 12:55:28 +0800

    Update route.js

--- src/app/api/ip/route.js
+++ src/app/api/ip/route.js
@@ -1,16 +1,27 @@
 import { NextResponse } from "next/server";
-import { headers } from 'next/headers'
 import { getRequestContext } from '@cloudflare/next-on-pages';
 
 // ...
 
-
 export const runtime = 'edge';
 export async function GET(request) {
   // 获取客户端的IP地址
-  // const { env, cf, ctx } = getRequestContext();
-  const ip = request.headers.get('x-forwarded-for') || request.headers.get('x-real-ip') || request.socket.remoteAddress;
-  const clientIp = ip ? ip.split(',')[0].trim() : 'IP not found';
+  const xForwardedFor = request.headers.get('x-forwarded-for');
+  const xRealIp = request.headers.get('x-real-ip');
+  const remoteAddress = request.socket.remoteAddress;
+
+  let ipList = [];
+  if (xForwardedFor) {
+    ipList = xForwardedFor.split(',').map(ip => ip.trim());
+  } else if (xRealIp) {
+    ipList = [xRealIp.trim()];
+  } else if (remoteAddress) {
+    ipList = [remoteAddress];
+  }
+
+  // 提取IPv4地址
+  const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
+  const clientIp = ipList.find(ip => ipv4Regex.test(ip)) || 'IP not found';
 
   return new Response(
     JSON.stringify({
@@ -22,13 +33,8 @@ export async function GET(request) {
         'content-type': 'application/json',
       },
     }
-  )
-
-
-
-
+  );
 }
-
 
 async function insertImageData(env, src, referer, ip, rating, time) {
   try {
@@ -37,6 +43,6 @@ async function insertImageData(env, src, referer, ip, rating, time) {
            VALUES ('${src}', '${referer}', '${ip}', ${rating}, 1, '${time}')`
     ).run();
   } catch (error) {
-
+    // 处理错误
   }
 }
