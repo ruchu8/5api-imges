a611662c08d92d0ff5dae4b5312b7c3b9626e1f7
ruchu8 <o0o08l@outlook.com>
2024-11-04 13:04:42 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 <footer className="w-full h-1/12 text-center bg-slate-200 flex flex-col justify-center items-center">
   <div>
     <p className="text-xs text-gray-500">
-      Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于 Cloudflare Pages，开源于
+      Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。本站会记录上传者IP地址。本程序基于 Cloudflare Pages，开源于
       <Link
         href="https://github.com/x-dr/telegraph-Image"
         className="text-blue-300 hover:text-red-900 ml-1"
