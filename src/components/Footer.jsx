dedb2496c475be0beab0b3d625a9792dc730aef0
ruchu8 <o0o08l@outlook.com>
2024-11-04 13:01:53 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,25 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-export default function Footer() {
-  return (
-    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
-      <div >
-        <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于Cloudflare Pages，开源于
-          <Link 
-          href="https://github.com/x-dr/telegraph-Image"
-          className="text-blue-300  hover:text-red-900 ml-1"
-          target="_blank"
-          rel="noopener noreferrer"
-          >GitHub Telegraph-Image</Link> </p>
-      </div>
-    </footer>
-  );
-}
+<footer className="w-full h-1/12 text-center bg-slate-200 flex flex-col justify-center items-center">
+  <div>
+    <p className="text-xs text-gray-500">
+      Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于 Cloudflare Pages，开源于
+      <Link
+        href="https://github.com/x-dr/telegraph-Image"
+        className="text-blue-300 hover:text-red-900 ml-1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub Telegraph-Image
+      </Link>
+    </p>
+    <ul className="text-xs text-gray-500 mt-2">
+      友情链接
+      <li className="list-inline-item"><a href="https://58.linkpc.net" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">免费API接口</a></li>
+      <li className="list-inline-item"><a href="http://ruchu888.ysepan.com" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">传奇射线补丁</a></li>
+      <li className="list-inline-item"><a href="https://592.us.kg" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">如初的博客</a></li>
+      <li className="list-inline-item"><a href="http://ruchu888.ysepan.com/" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">如初的网盘</a></li>
+      <li className="list-inline-item"><a href="http://wcq.us.kg/" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">传奇知识</a></li>
+      <li className="list-inline-item"><a href="https://api.592.us.kg/" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">随机图片API</a></li>
+      <li className="list-inline-item"><a href="https://img.592.us.kg/" target="_blank" className="text-blue-300 hover:text-red-900 ml-1">图床上传</a></li>
+    </ul>
+  </div>
+</footer>
